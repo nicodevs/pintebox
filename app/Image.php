8bad349bc182e811a528392bf5407a3efc1205b3
Nico Beta <nico1485@gmail.com>
2018-07-07 18:36:17 -0300

    Add methods to read feed and store image file

--- app/Image.php
+++ app/Image.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'url', 'pub_date', 'board_name', 'board_url'];
 }
--- database/migrations/2018_07_07_192259_create_images_table.php
+++ database/migrations/2018_07_07_192259_create_images_table.php
@@ -15,7 +15,11 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->string('url')->unique();
+            $table->string('pub_date');
+            $table->string('board_name');
+            $table->string('board_url');
             $table->boolean('imported')->default(0);
             $table->timestamps();
         });
